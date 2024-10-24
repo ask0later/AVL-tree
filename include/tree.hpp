276940be28107e574986db609a7757d3b24eeecb
Yaroslav Kuznetsov <asklater@bk.ru>
2024-10-24 21:15:22 +0300

    implemented balancing, tests passed

--- include/tree.hpp
+++ include/tree.hpp
@@ -16,13 +16,12 @@ namespace trees {
     
     public:
         Node() = delete;
-        Node(KeyT key) : key_(key) {}
+        Node(KeyT key) : key_(key), height_(1) {}
         Node(KeyT key, Node* parent, Node* left = nullptr, Node* right = nullptr) : key_(key),
                                                                                     parent_(parent),
                                                                                     left_(left),
                                                                                     right_(right),
-                                                                                    height_(1),
-                                                                                    balance_factor_(0) {}
+                                                                                    height_(1) {}
 
         ~Node()
         {
@@ -30,31 +29,120 @@ namespace trees {
             delete right_;
         }
 
-        bool insert(KeyT key)
+        static int height(Node *node) 
+        { 
+            return node ? node->height_ : 0;
+        } 
+
+        static Node *rotate_right(Node *y) 
+        { 
+            Node *x = y->left_; 
+            Node *T2 = x->right_; 
+
+            x->right_ = y;
+            y->left_ = T2; 
+
+            y = update_node(y);
+            x = update_node(x);
+
+            if (T2)
+                T2->parent_ = y;
+
+            x->parent_ = y->parent_;
+            y->parent_ = x;
+
+            return x; 
+        } 
+ 
+        static Node *rotate_left(Node *x)
+        { 
+            Node *y = x->right_; 
+            Node *T2 = y->left_; 
+
+            y->left_ = x;
+            x->right_ = T2; 
+
+            x = update_node(x);
+            y = update_node(y);
+
+            if (T2)
+                T2->parent_ = x;
+            y->parent_ = x->parent_;
+            x->parent_ = y;
+
+            return y; 
+        } 
+
+        static int balance_factor(Node *node) 
+        { 
+            return node ? (height(node->left_) - height(node->right_)) : 0;
+        }
+
+        static Node *update_node(Node *node)
         {
-            if (key == key_)
-                return false;
+            node->height_ = 1 + std::max(height(node->left_), height(node->right_));
+            return node; 
+        }
+
+        static Node *balance_node(KeyT key, Node* node)
+        {
+            int balance = balance_factor(node); 
+
+            if (balance > 1 && key < node->left_->key_) 
+                return rotate_right(node); 
+
+            if (balance < -1 && key > node->right_->key_) 
+                return rotate_left(node); 
+
+            if (balance > 1 && key > node->left_->key_)
+            { 
+                node->left_ = rotate_left(node->left_); 
+                return rotate_right(node); 
+            } 
+
+            if (balance < -1 && key < node->right_->key_)
+            { 
+                node->right_ = rotate_right(node->right_); 
+                return rotate_left(node); 
+            }
+
+            return node;
+        }
+
+        static Node *insert(KeyT key, Node *node, Node *parent) 
+        { 
+            if (node == nullptr) 
+                return new Node(key, parent);
+
+            if (key < node->key_)
+                node->left_ = insert(key, node->left_, node); 
+            else if (key > node->key_) 
+                node->right_ = insert(key, node->right_, node); 
+            else 
+                return node; 
             
-            if (key < key_) // left
-            {
-                if (left_ == nullptr)
-                {
-                    left_ = new Node(key, this);
-                    return true;
-                }
-                
-                return left_->insert(key);
-            }
-            else // right
-            {
-                if (right_ == nullptr)
-                {
-                    right_ = new Node(key, this);
-                    return true;
-                }
-                
-                return right_->insert(key);
-            }
+            node = update_node(node);
+            node = balance_node(key, node);
+
+            return node; 
+        } 
+        
+        void print_in_preorder() const
+        {
+            if (parent_)
+                std::cout << "#" << key_ << " " << parent_->key_ << " " << height_ << "#" << "(";
+            else
+                std::cout << "#" << key_ << " " << height_ << "#" << "(";
+
+            if (left_)
+                left_->print_in_preorder();
+            
+            std::cout << ", ";
+
+            if (right_)
+                right_->print_in_preorder();
+        
+            std::cout << ")";
         }
 
     private:
@@ -63,7 +151,6 @@ namespace trees {
         Node* right_ = nullptr;
         Node* parent_ = nullptr;
         int height_;
-        int balance_factor_;
         KeyT key_; 
 
     }; // class Node
@@ -110,14 +197,11 @@ namespace trees {
                     return *this;
                 }
 
-                if (node_->parent_ == nullptr)
-                {
-                    return *this;
-                }
+                assert(node_->parent_ != nullptr);
 
                 Node<KeyT> *parent = node_->parent_;
                 Node<KeyT> *current = node_;
-
+                
                 while (current == parent->right_)
                 {
                     current = parent;
@@ -154,10 +238,7 @@ namespace trees {
                     return *this;
                 }
 
-                if (node_->parent_ == nullptr)
-                {
-                    return *this;
-                }
+                assert(node_->parent_ != nullptr);
 
                 Node<KeyT> *parent = node_->parent_;
                 Node<KeyT> *current = node_;
@@ -183,6 +264,9 @@ namespace trees {
 
             KeyT operator*()
             {
+                if (node_ == nullptr)
+                    return 7777;
+
                 return node_->key_;
             }
         private:
@@ -207,18 +291,7 @@ namespace trees {
 
         void insert(KeyT key)
         {
-            if (root_ == nullptr)
-            {
-                root_ = new Node(key);
-                size_++;
-            }
-            else
-            {
-                if (root_->insert(key))
-                {
-                    size_++;
-                }
-            }
+            root_ = Node<KeyT>::insert(key, root_, nullptr);
         }
 
         Iterator lower_bound(KeyT key) const
@@ -226,7 +299,8 @@ namespace trees {
             Iterator it = begin();
             Iterator end_it = end();
 
-            for (; it != end_it && *it <= key; ++it) ;
+            for (; it != end_it && *it <= key; ++it)
+                ;
 
             if (it == end_it)
                 return end();
@@ -239,7 +313,8 @@ namespace trees {
             Iterator it = back();
             Iterator end_it = end();
 
-            for (; it != end_it && *it >= key; --it) ;
+            for (; it != end_it && *it >= key; --it)
+                ;
 
             if (it == end_it)
                 return end();
@@ -262,10 +337,10 @@ namespace trees {
         {
             if (key1 > key2 || root_ == nullptr)
                 return 0;
-        
+
             Iterator it1 = lower_bound(key1);
             Iterator it2 = upper_bound(key2);
-            
+
             return distance(it1, it2);
         }
 
@@ -302,6 +377,12 @@ namespace trees {
         Iterator end() const
         {
             return Iterator{nullptr, this};
+        }
+
+        void print() const 
+        {
+            root_->print_in_preorder();
+            std::cout << std::endl;
         }
 
     private:
