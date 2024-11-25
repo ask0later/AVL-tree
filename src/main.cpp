7baf219690bdca3a114ecf5f1f23b37896e5124e
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2024-11-25 16:46:49 +0300

    refactor input_output

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,26 @@
 #include <cassert>
 
 #include "tree.hpp"
-#include "input_output.hpp"
+#include "process_queries.hpp"
 
 int main()
 {
-    trees::AVLtree<int> tree;
-    std::vector<size_t> answer_tree;
-
-    auto distance = [](trees::AVLtree<int> &tree, int key1, int key2)
-                    {return tree.get_num_elems_from_diapason_fast(key1, key2);};
-    bool result = io::process_input<int, trees::AVLtree<int>>(tree, answer_tree, distance, std::cin);
-
+    std::vector<query::Query<int>> queries;
+    bool result = query::process_input<int>(queries, std::cin);
     if (!result)
     {
         std::cout << "Incorrect input" << std::endl;
         return 1;
     }
 
-    io::print_answers(answer_tree);
+    trees::AVLtree<int> tree;
+
+    auto distance = [](trees::AVLtree<int> &tree, int key1, int key2)
+                    {return tree.get_num_elems_from_diapason_fast(key1, key2);};
+
+    std::vector<size_t> answer_tree = query::get_answers<int>(tree, queries, distance);
+
+    query::print_answers(answer_tree);
 
     return 0;
 }
